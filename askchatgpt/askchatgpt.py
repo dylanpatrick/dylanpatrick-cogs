53fd7ae346a2c00137ca51f76a9a2cbc6a50e9a0
dylanpatrick <130814386+dylanpatrick@users.noreply.github.com>
2023-11-20 00:43:47 +0000

    Update askchatgpt.py

--- askchatgpt/askchatgpt.py
+++ askchatgpt/askchatgpt.py
@@ -1,17 +1,16 @@
 import openai
 from redbot.core import commands, Config
 from redbot.core.bot import Red
-from redbot.core.data_manager import cog_data_path
-import asyncio
 
-class AskChatGPT(commands.Cog):
+class ChatGPT(commands.Cog):
     """A Redbot cog to interact with OpenAI's ChatGPT."""
 
     def __init__(self, bot: Red):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890, force_registration=True)
         default_global = {
-            "api_key": None
+            "api_key": None,
+            "model": "gpt-3.5-turbo"  # Default model
         }
         self.config.register_global(**default_global)
 
@@ -19,6 +18,7 @@ class AskChatGPT(commands.Cog):
     async def askgpt(self, ctx, *, query: str):
         """Ask a question to ChatGPT."""
         api_key = await self.config.api_key()
+        model = await self.config.model()
         if not api_key:
             await ctx.send("OpenAI API key is not set. Please set it using `setapikey` command.")
             return
@@ -26,7 +26,7 @@ class AskChatGPT(commands.Cog):
         try:
             openai.api_key = api_key
             response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
+                model=model,
                 messages=[{"role": "system", "content": "You are a helpful assistant."},
                           {"role": "user", "content": query}]
             )
@@ -43,13 +43,18 @@ class AskChatGPT(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def getapikey(self, ctx):
-        """Get the current OpenAI API key (Owner only)."""
+    async def setmodel(self, ctx, new_model: str):
+        """Set the OpenAI model (Owner only)."""
+        await self.config.model.set(new_model)
+        await ctx.send(f"Model updated to {new_model}.")
+
+    @commands.command()
+    @commands.is_owner()
+    async def getsettings(self, ctx):
+        """Get the current settings (Owner only)."""
         api_key = await self.config.api_key()
-        if api_key:
-            await ctx.send(f"The current API key is: {api_key}")
-        else:
-            await ctx.send("No API key is set.")
+        model = await self.config.model()
+        await ctx.send(f"Current API key: {api_key}\nCurrent model: {model}")
 
 def setup(bot):
-    bot.add_cog(AskChatGPT(bot))
+    bot.add_cog(ChatGPT(bot))
