0c05c7fe7402daa04fd7803eb4fc2089f68814e0
dylanpatrick <130814386+dylanpatrick@users.noreply.github.com>
2023-12-03 17:32:26 -0600

    Create thvalley.py

---
+++ thevalley/thvalley.py
@@ -0,0 +1,32 @@
+from redbot.core import commands
+import discord
+import asyncio
+
+class TheValley(commands.Cog):
+    """Cog to restrict users to post in 'The Valley' for 10 seconds."""
+
+    def __init__(self, bot):
+        self.bot = bot
+      
+@commands.command()
+@commands.has_permissions(manage_roles=True)
+async def valleykick(self, ctx, member: discord.Member):
+    """Restrict a user to post in 'The Valley' for 10 seconds."""
+    valley_channel = discord.utils.get(ctx.guild.text_channels, name='The Valley')
+    if valley_channel is None:
+        await ctx.send("Channel 'The Valley' not found.")
+        return
+
+    # Create a temporary overwrite
+    overwrite = discord.PermissionOverwrite()
+    overwrite.send_messages = True
+    await valley_channel.set_permissions(member, overwrite=overwrite)
+
+    # Reset permissions after 10 seconds
+    await asyncio.sleep(10)
+    await valley_channel.set_permissions(member, overwrite=None)
+    await ctx.send(f"{member} can now post in other channels again.")
+
+def setup(bot):
+    bot.add_cog(ValleyRestriction(bot))
+
